5104dd8be291ad500974d8b7709227608574ec30
Daniel Prokesch <daniel.prokesch@gmail.com>
2017-02-10 02:48:26 +0100

    fix path reading

--- fpcc-map.c
+++ fpcc-map.c
@@ -16,15 +16,11 @@
 
 const char *program_name = "fpcc-map";
 
-struct hashes {
-  uint32_t count;
-  hash_entry_t *arr;
-} hashes1, hashes2;
-
-struct paths {
-  uint32_t count;
-  char **arr;
-} paths1, paths2;
+struct index {
+  uint32_t hash_cnt, path_cnt;
+  hash_entry_t *hashes;
+  char **paths;
+};
 
 
 void usage(void)
@@ -34,34 +30,36 @@ void usage(void)
 }
 
 
-void load_file(const char *fname, struct hashes *hashes, struct paths *paths)
+void load_file(const char *fname, struct index *idx)
 {
   FILE *f = fopen(fname, "r");
   if (f == NULL) {
     goto fail;
   }
-  if (fread(&hashes->count, sizeof hashes->count, 1, f) != 1) {
+  if (fread(&idx->hash_cnt, sizeof idx->hash_cnt, 1, f) != 1) {
     goto fail;
   }
-  hashes->arr = malloc((hashes->count) * sizeof(hash_entry_t));
-  if (hashes->arr == NULL) {
+  idx->hashes = malloc((idx->hash_cnt) * sizeof(hash_entry_t));
+  if (idx->hashes == NULL) {
     error_exit("can't allocate buffer");
   }
-  if (fread(hashes->arr, sizeof(hash_entry_t),
-        hashes->count, f) != hashes->count) {
+  if (fread(idx->hashes, sizeof(hash_entry_t),
+        idx->hash_cnt, f) != idx->hash_cnt) {
     goto fail;
   }
   // now the paths
-  if (fread(&paths->count, sizeof paths->count, 1, f) != 1) {
+  if (fread(&idx->path_cnt, sizeof idx->path_cnt, 1, f) != 1) {
     goto fail;
   }
-  paths->arr = calloc(paths->count, sizeof(char *));
-  if (paths->arr == NULL) {
+  // one more field that stays NULL
+  idx->paths = calloc(idx->path_cnt + 1, sizeof(char *));
+  if (idx->paths == NULL) {
     error_exit("can't allocate buffer");
   }
   size_t len = 0;
-  char **pptr = paths->arr;
-  while (getdelim(pptr++, &len, '\0', f) != -1);
+  for (int i = 0; i < idx->path_cnt; i++) {
+    (void) getdelim(&idx->paths[i], &len, '\0', f);
+  }
 
   (void) fclose(f);
   return;
@@ -87,26 +85,30 @@ int main(int argc, char *argv[])
   // exactly two arguments
   if (argc - optind != 2) usage();
 
-  load_file(argv[optind], &hashes1, &paths1);
+  struct index idx1, idx2;
+  load_file(argv[optind], &idx1);
+  load_file(argv[optind + 1], &idx2);
 
 
 
   // iterate in input order
-  int k = hashes1.arr[0].next;
+  int k = idx1.hashes[0].next;
   while (k > 0) {
-    hash_entry_t *he = &hashes1.arr[k];
+    hash_entry_t *he = &idx1.hashes[k];
     DBG("%016lx l%d f%d n%d\n", he->hash, he->linepos,
         he->filecnt, he->next);
     k = he->next;
   }
-  free(hashes1.arr);
+  free(idx1.hashes);
 
-
-  for (int i = 0; i < paths1.count; i++) {
-    DBG("Path: %s\n", paths1.arr[i]);
-    free(paths1.arr[i]);
+  //for (int i = 0; i < idx1.path_cnt; i++) {
+  for (char **pptr = idx1.paths; *pptr != NULL; pptr++) {
+    //DBG("Path: %s\n", idx1.paths[i]);
+    DBG("Path: %s\n", *pptr);
+    //free(idx1.paths[i]);
+    free(*pptr);
   }
-  free(paths1.arr);
+  free(idx1.paths);
 
   exit(EXIT_SUCCESS);
 }
